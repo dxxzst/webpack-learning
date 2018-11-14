d2092525d41c94f8b389c7b095fda6bed41da9bb
XueSong <song.xue@seek-data.com>
2018-11-14 14:52:43 +0800

    extract-text-webpack-plugin

--- 1.0 webpack basic setting/webpack.config.js
+++ 1.0 webpack basic setting/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+// 提取css的插件
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './js/main.js',
@@ -12,12 +14,22 @@ module.exports = {
         rules: [{
             // 用正则去匹配以 .css结尾的文件，然后需要使用loader进行转换
             test: /\.css$/,
-            use: ['style-loader', { //'css-loader?minimize'
-                loader: 'css-loader',
-                options: {
-                    minimize: true
-                }
-            }] //Loader的执行顺序是由后往前的 
+            // use: ['style-loader', { //'css-loader?minimize'
+            //     loader: 'css-loader',
+            //     options: {
+            //         minimize: true
+            //     }
+            // }] //Loader的执行顺序是由后往前的 
+            loaders: ExtractTextPlugin.extract({
+                // 转换 .css文件需要使用的Loader
+                use: ['css-loader']
+            })
         }]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            // 从js文件中提取出来的 .css文件的名称
+            filename: `main.css`
+        })
+    ]
 }
