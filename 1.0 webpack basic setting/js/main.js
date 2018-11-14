065cff19edaf210a43f0833624cb2cbbe599266e
XueSong <song.xue@seek-data.com>
2018-11-14 14:24:41 +0800

    add loader

--- 1.0 webpack basic setting/js/main.js
+++ 1.0 webpack basic setting/js/main.js
@@ -1,3 +1,4 @@
+require('../styles/main.css');
 const demo1Func = require('./demo1.js');
 
 demo1Func();
--- 1.0 webpack basic setting/webpack.config.js
+++ 1.0 webpack basic setting/webpack.config.js
@@ -7,5 +7,17 @@ module.exports = {
         filename: 'bundle.js',
         // 将输出的文件都放在dist目录下
         path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [{
+            // 用正则去匹配以 .css结尾的文件，然后需要使用loader进行转换
+            test: /\.css$/,
+            use: ['style-loader', { //'css-loader?minimize'
+                loader: 'css-loader',
+                options: {
+                    minimize: true
+                }
+            }] //Loader的执行顺序是由后往前的 
+        }]
     }
 }
