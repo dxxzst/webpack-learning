23236f86c49d25c9839e1795317431590828f134
XueSong <song.xue@seek-data.com>
2018-11-16 13:30:35 +0800

    test babel

--- 3.0 babel-in-webpack/js/demo1.js
+++ 3.0 babel-in-webpack/js/demo1.js
@@ -1,1 +1,27 @@
-console.log('demo1 log');
+//console.log('demo1 log');
+
+export default class User {
+    constructor(userName, email) {
+        this._userName = userName;
+        this._email = email;
+    }
+
+    changeEmail(newEmail) {
+        this._email = newEmail;
+    }
+
+    static register(...args) {
+        return new User(...args);
+    }
+
+    get info() {
+        return this._userName + ' ' + this._email;
+    }
+
+    get name() {
+        return this._userName;
+    }
+    set name(name) {
+        this._userName = name.slice(0, 1).toUpperCase().concat(name.slice(1));
+    }
+}
--- 3.0 babel-in-webpack/js/main.js
+++ 3.0 babel-in-webpack/js/main.js
@@ -1,17 +1,25 @@
 require('../styles/main.css');
 
-import demo1 from './demo1.js';
+import User from './demo1.js';
 
-function* g() {
-    yield 'a';
-    yield 'b';
-    yield 'c';
-    return 'ending';
-}
+// function* g() {
+//     yield 'a';
+//     yield 'b';
+//     yield 'c';
+//     return 'ending';
+// }
 
-var gen = g();
-console.log(gen.next()); // 返回Object {value: "a", done: false}
+// var gen = g();
+// console.log(gen.next()); // 返回Object {value: "a", done: false}
 
-for (let a of [1, 2, 3, 4]) {
-    console.log(a); // 打印出 1, 2, 3, 4
-}
+// for (let a of [1, 2, 3, 4]) {
+//     console.log(a); // 打印出 1, 2, 3, 4
+// }
+
+// 使用
+//let user = new User("zen", "ihuangmx@qq.com");
+let user = User.register("zen", "ihuangmx@qq.com");
+console.log(user.info); // zen ihuangmx@qq.com
+user.name = "jack";
+//console.log(user.name); 
+console.log(user.info); // Jack ihuangmx@qq.com
